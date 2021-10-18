a1b3af0e6c84cd32850c7ba850287b7051384543
hank <steven-online@hotmail.com>
2021-10-18 16:23:09 +0800

    feat: 新增自动化deploy workflows

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'dumi';
+import { defineConfig } from 'dumi'
 
 export default defineConfig({
   title: 'simple-mobile',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'lean',
+        camel2DashComponentName: false,
+        customStyleName: name => {
+          return `./style/index.scss` // 注意：这里 ./ 不可省略
+        },
+      },
+      'lean',
+    ],
+  ],
   // more config: https://d.umijs.org/config
-});
+})
