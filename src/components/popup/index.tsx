acf331db68a6fdf924a620aa16e42d468501a584
hank <steven-online@hotmail.com>
2021-10-19 18:36:42 +0800

    feat：新增 popup 组件

---
+++ src/components/popup/index.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState, useRef, FC } from 'react'
+import classNames from 'classnames'
+import { useLockBodyScroll } from 'react-use'
+import { CSSTransition } from 'react-transition-group'
+import {
+  ElementProps,
+  useInitialized,
+  mergeProps,
+  GetContainer,
+  renderToContainer,
+} from '../../utils'
+import Mask from '../mask'
+import './popup.scss'
+const classPrefix = `ah-popup`
+
+export type PopupProps = {
+  visible?: boolean
+  mask?: boolean
+  onMaskClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  destroyOnClose?: boolean
+  forceRender?: boolean
+  getContainer?: GetContainer
+  afterShow?: () => void
+  afterClose?: () => void
+  position?: 'bottom' | 'top' | 'left' | 'right'
+  bodyClassName?: string
+  bodyStyle?: React.CSSProperties
+  maskClassName?: string
+  maskStyle?: React.CSSProperties
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  disableBodyScroll?: boolean
+} & ElementProps
+
+const defaultProps = {
+  position: 'bottom',
+  visible: false,
+  getContainer: () => document.body,
+  mask: true,
+  disableBodyScroll: true,
+}
+
+const Popup: FC<PopupProps> = p => {
+  const props = mergeProps(defaultProps, p)
+  // 动画执行完，才隐藏最外层
+  const [hidden, setHidden] = useState(!props.visible)
+
+  useEffect(() => {
+    if (props.visible) {
+      setHidden(false)
+    }
+  }, [props.visible])
+
+  const afterClose = () => {
+    setHidden(true)
+    props.afterClose?.()
+  }
+
+  const cls = classNames(classPrefix, props.className, {
+    [`${classPrefix}-hidden`]: hidden,
+  })
+
+  const bodyCls = classNames(
+    `${classPrefix}-body`,
+    props.bodyClassName,
+    {
+      [`${classPrefix}-body-hidden`]: !props.visible,
+    },
+    `${classPrefix}-body-position-${props.position}`
+  )
+
+  const initialized = useInitialized(props.visible || props.forceRender)
+
+  const ref = useRef<HTMLDivElement>(null)
+
+  useLockBodyScroll(props.visible, ref)
+
+  const node = (
+    <div className={cls} style={props.style} onClick={props.onClick}>
+      {props.mask && (
+        <Mask
+          visible={props.visible}
+          onMaskClick={props.onMaskClick}
+          className={props.maskClassName}
+          style={props.maskStyle}
+          disableBodyScroll={props.disableBodyScroll}
+        />
+      )}
+      <CSSTransition
+        classNames={`${classPrefix}-body`}
+        in={props.visible}
+        timeout={200}
+        onEntered={props.afterShow}
+        onExited={afterClose}
+        unmountOnExit={props.destroyOnClose}
+      >
+        <div className={bodyCls} style={props.bodyStyle} ref={ref}>
+          {initialized && props.children}
+        </div>
+      </CSSTransition>
+    </div>
+  )
+
+  return renderToContainer(props.getContainer, node)
+}
+
+export default Popup
