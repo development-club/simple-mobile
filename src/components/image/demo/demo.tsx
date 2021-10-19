10b3d09b1a917456d59b97a2e75fcd07e2d275c7
hank <steven-online@hotmail.com>
2021-10-19 18:37:00 +0800

    feat： 新增 Image组件

---
+++ src/components/image/demo/demo.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from 'simple-mobile/components/image'
+import Space from '../../space'
+import { DemoBlock } from '../../../demos'
+
+const demoSrc =
+  'http://car3.autoimg.cn/cardfs/product/g20/M0C/06/2A/248x186_q95_autohomecar__ChsElWB-guGAMHQMAAqaXJvG3Hs873.jpg'
+
+export default () => {
+  return (
+    <>
+      <DemoBlock title='基础用法'>
+        <Image src={demoSrc} />
+      </DemoBlock>
+      <DemoBlock title='多种填充模式'>
+        <Space wrap direction='horizontal'>
+          <Image src={demoSrc} width={100} height={100} fit='fill' />
+          <Image src={demoSrc} width={100} height={100} fit='contain' />
+          <Image src={demoSrc} width={100} height={100} fit='cover' />
+          <Image src={demoSrc} width={100} height={100} fit='scale-down' />
+          <Image src={demoSrc} width={100} height={100} fit='none' />
+        </Space>
+      </DemoBlock>
+      <DemoBlock title='自定义圆角'>
+        <Space wrap direction='horizontal'>
+          <Image
+            src={demoSrc}
+            width={64}
+            height={64}
+            fit='cover'
+            style={{ borderRadius: 4 }}
+          />
+          <Image
+            src={demoSrc}
+            width={64}
+            height={64}
+            fit='cover'
+            style={{ borderRadius: 8 }}
+          />
+          <Image
+            src={demoSrc}
+            width={64}
+            height={64}
+            fit='cover'
+            style={{ borderRadius: 32 }}
+          />
+        </Space>
+      </DemoBlock>
+      <DemoBlock title='加载失败'>
+        <Image src='/404' width={100} height={100} />
+      </DemoBlock>
+    </>
+  )
+}
---
+++ src/components/image/index.tsx
@@ -0,0 +1,99 @@
+import React, { FC, ReactNode, useState } from 'react'
+import classNames from 'classnames'
+import { ElementProps } from 'simple-mobile/utils/element-props'
+import { mergeProps } from 'simple-mobile/utils/with-default-props'
+import Icon from 'simple-mobile/components/icon'
+import './image.scss'
+const classPrefix = `ah-image`
+
+export type ImageProps = {
+  src: string
+  alt?: string
+  width?: number | string
+  height?: number | string
+  fit?: 'contain' | 'cover' | 'fill' | 'none' | 'scale-down'
+  style?: React.CSSProperties
+  placeholder?: ReactNode
+  fallback?: ReactNode
+  onClick?: (event: React.SyntheticEvent<HTMLImageElement, Event>) => void
+  onError?: (event: React.SyntheticEvent<HTMLImageElement, Event>) => void
+} & ElementProps &
+  Pick<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    | 'crossOrigin'
+    | 'decoding'
+    | 'loading'
+    | 'referrerPolicy'
+    | 'sizes'
+    | 'srcSet'
+    | 'useMap'
+  >
+
+const defaultProps = {
+  fit: 'fill',
+  placeholder: (
+    <div className={`${classPrefix}-tip`}>
+      <Icon type='picture' className='anticon' />
+    </div>
+  ),
+  fallback: (
+    <div className={`${classPrefix}-tip`}>
+      <Icon type='disconnect' className='anticon' />
+    </div>
+  ),
+}
+
+const Image: FC<ImageProps> = p => {
+  const props = mergeProps(defaultProps, p)
+  const [loaded, setLoaded] = useState(false)
+  const [failed, setFailed] = useState(false)
+  function renderInner() {
+    if (failed) {
+      return props.fallback
+    }
+    return (
+      <>
+        {!loaded && props.placeholder}
+        <img
+          className={`${classPrefix}-img`}
+          src={props.src}
+          alt={props.alt}
+          onClick={props.onClick}
+          onLoad={() => {
+            setLoaded(true)
+          }}
+          onError={e => {
+            setFailed(true)
+            props.onError?.(e)
+          }}
+          style={{
+            objectFit: props.fit,
+            display: loaded ? 'block' : 'none',
+          }}
+          crossOrigin={props.crossOrigin}
+          decoding={props.decoding}
+          loading={props.loading}
+          referrerPolicy={props.referrerPolicy}
+          sizes={props.sizes}
+          srcSet={props.srcSet}
+          useMap={props.useMap}
+        />
+      </>
+    )
+  }
+
+  return (
+    <div
+      className={classNames(classPrefix, props.className)}
+      style={{
+        width: props.width,
+        height: props.height,
+        ...props.style,
+      }}
+    >
+      {renderInner()}
+    </div>
+  )
+}
+
+export default Image
