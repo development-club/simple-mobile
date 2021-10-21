fbe85c9a4424e5247d0d2e85dc2a3c7f9b04c04e
hank <steven-online@hotmail.com>
2021-10-21 17:05:40 +0800

    fix: 开启 mfsu 打包加速 并调整主题样式

--- config/config.ts
+++ config/config.ts
@@ -77,6 +77,10 @@ const menus = {
 //     children: item.children,
 //   }))
 // }
+const mfsu =
+  process.env.NODE_ENV === 'production'
+    ? { production: { output: '.mfsu-production' } }
+    : {} // 开启基于webpack5的新特性的打包方案
 
 export default {
   mode: 'site',
@@ -112,11 +116,12 @@ export default {
       // umi-hd 的 750 高清方案（默认值）
       // [{ mode: 'vw', options: [100, 750] }],
       // 根据不同的设备屏幕宽度断点切换高清方案
-      // rules: [
-      //   { maxWidth: 375, mode: 'vw', options: [100, 750] },
-      //   { minWidth: 376, maxWidth: 750, mode: 'vw', options: [100, 1500] },
-      // ],
+      rules: [
+        { maxWidth: 375, mode: 'vw', options: [100, 750] },
+        { minWidth: 376, maxWidth: 750, mode: 'vw', options: [100, 1500] },
+      ],
       // 更多 rule 配置访问 https://github.com/umijs/dumi/blob/master/packages/theme-mobile/src/typings/config.d.ts#L7
     },
   },
+  mfsu: mfsu,
 }
