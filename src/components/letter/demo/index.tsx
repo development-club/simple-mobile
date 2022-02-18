cc53e39f612b902a64b22bdb8d025c9d01ac9a11
hank <steven-online@hotmail.com>
2022-02-18 12:27:54 +0800

    ffeat

---
+++ src/components/letter/demo/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Letter, Space } from 'simple-mobile'
+import { DemoBlock } from 'demos'
+
+export default () => {
+  return (
+    <>
+      <DemoBlock title='基本用法'>
+        <Space>
+          <Letter />
+        </Space>
+      </DemoBlock>
+    </>
+  )
+}
