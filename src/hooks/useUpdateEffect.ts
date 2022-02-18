cb8a6952f6b99eab75882d3fc0443cf5718005ea
hank <steven-online@hotmail.com>
2022-02-18 14:09:45 +0800

    feat: add hooks  useUpdateEffect

---
+++ src/hooks/useUpdateEffect.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { useFirstMountState } from './useFirstMountState'
+
+export const useUpdateEffect: typeof useEffect = (effect, deps) => {
+  const isFirstMount = useFirstMountState()
+
+  useEffect(() => {
+    if (!isFirstMount) {
+      return effect()
+    }
+  }, deps)
+}
+
+export default useUpdateEffect
